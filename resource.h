113cb020cad112b65a0e41a51afd183c84f7d53e
qingfan123 <3094056092@qq.com>
2020-05-21 19:47:38 +0800

    resource

---
+++ resource.h
@@ -0,0 +1,54 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MortgageCalculator.rc 使用
+//
+#define IDC_MYICON                      2
+#define IDD_MORTGAGECALCULATOR_DIALOG   102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MORTGAGECALCULATOR          107
+#define IDI_SMALL                       108
+#define IDC_MORTGAGECALCULATOR          109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_BUTTON1                     1000
+#define IDC_BUTTON2                     1001
+#define IDC_EDIT1                       1002
+#define IDC_RADIO1                      1003
+#define IDC_RADIO2                      1004
+#define IDC_EDIT2                       1005
+#define IDC_EDIT3                       1006
+#define IDC_EDIT4                       1007
+#define IDC_EDIT6                       1009
+#define IDC_BUTTON3                     1010
+#define IDC_RADIO3                      1011
+#define IDC_RADIO4                      1012
+#define IDC_EDIT7                       1013
+#define IDC_BUTTON4                     1014
+#define IDC_EDIT8                       1015
+#define IDC_EDIT9                       1016
+#define IDC_EDIT10                      1017
+#define IDC_EDIT11                      1018
+#define IDC_RADIO5                      1019
+#define IDC_RADIO6                      1020
+#define IDC_EDIT12                      1021
+#define IDC_EDIT13                      1022
+#define IDC_EDIT16                      1025
+#define IDC_EDIT17                      1026
+#define IDC_EDIT18                      1027
+#define IDC_EDIT19                      1028
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
